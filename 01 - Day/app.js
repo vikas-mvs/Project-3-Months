6d243bc3dfd9f028b2d12c2963ba7b7c9d87f0e4
Vikas Kumar <vikasstm2003@gmail.com>
2026-05-26 09:25:41 +0530

    update app.js

--- 01 - Day/app.js
+++ 01 - Day/app.js
@@ -1,4 +1,18 @@
-function magic () {
-    alert("Now, Opening Instagram Id of Harshit.csv");
-    window.location = "https://www.instagram.com/harshit.csv/"
+function magic() {
+    let result = confirm("Now, Opening Instagram Id of Harshit.csv");
+
+    if(result === true){
+        window.location.href = "https://www.instagram.com/harshit.csv/";
+    }
+    else{
+        alert("Pleeeaaassee!!!..... Check.");
+    }
 }
+// // # Short form
+// function magic() {
+//     if(confirm("Open Instagram Id of Harshit.csv?")){
+//         window.location.href = "https://www.instagram.com/harshit.csv/";
+//     } else {
+//         alert("Pleeeaaassee!!!..... Check.");
+//     }
+// }
