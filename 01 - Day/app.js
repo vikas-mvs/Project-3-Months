d509531d295291a842745654fbb4166060a0f958
Vikas Kumar <vikasstm2003@gmail.com>
2026-05-26 17:14:30 +0530

    2nd Day Project

--- 01 - Day/app.js
+++ 01 - Day/app.js
@@ -8,11 +8,11 @@ function magic() {
         alert("Pleeeaaassee!!!..... Check.");
     }
 }
-// // # Short form
-// function magic() {
-//     if(confirm("Open Instagram Id of Harshit.csv?")){
-//         window.location.href = "https://www.instagram.com/harshit.csv/";
-//     } else {
-//         alert("Pleeeaaassee!!!..... Check.");
-//     }
-// }
+// # Short form
+function magic() {
+    if(confirm("Open Instagram Id of Harshit.csv?")){
+        window.location.href = "https://www.instagram.com/harshit.csv/";
+    } else {
+        alert("Pleeeaaassee!!!..... Check.");
+    }
+}
